cec374a46d2032d05fb2fc31f64992a8dd185705
Katie <katie@morpheuslab.co>
2018-12-28 16:52:30 -0500

    Updates

---
+++ precache-manifest.12c51fd9f4232868ffff87472aa6ef09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "a457d0806209891462c7",
+    "url": "/robofriends/static/js/main.a457d080.chunk.js"
+  },
+  {
+    "revision": "6fe3c5d14265b4f8d629",
+    "url": "/robofriends/static/js/1.6fe3c5d1.chunk.js"
+  },
+  {
+    "revision": "a457d0806209891462c7",
+    "url": "/robofriends/static/css/main.810188cf.chunk.css"
+  },
+  {
+    "revision": "6fe3c5d14265b4f8d629",
+    "url": "/robofriends/static/css/1.9978c924.chunk.css"
+  },
+  {
+    "revision": "e339f96bc9fe48a2c7c3e3bf86293256",
+    "url": "/robofriends/index.html"
+  }
+];
