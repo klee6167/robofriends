a98f7870cb5f1ffa31d3470a73ecbf34d1f2b98b
Katie <katie@morpheuslab.co>
2018-12-29 10:02:46 -0500

    testing Card, CardList and CounterButton

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -22,9 +22,12 @@ class CounterButton extends Component {
 	}
 
 	render() {
-		console.log('CounterButton');
 		return (
-			<button color={this.props.color} onClick={this.updateCount}>Count: {this.state.count}</button>
+			<button 
+				id='counter'
+				color={this.props.color} 
+				onClick={this.updateCount}>Count: {this.state.count}
+			</button>
 		);
 	}
 }
